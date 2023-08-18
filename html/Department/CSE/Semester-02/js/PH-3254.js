423f0b3108586f0a05d2c3e848b63a950e475794
Devsk001 <srik3713@gmail.com>
2023-08-18 22:48:21 +0530

    PH-3254 Video Added | Video Link Auto Search Added In JS File | Takes the Topic Tittle andperformSearch

--- html/Department/CSE/Semester-02/js/PH-3254.js
+++ html/Department/CSE/Semester-02/js/PH-3254.js
@@ -1,11 +1,27 @@
 // todo ShowVideo Funciton 
 
- function showVideo(unitId) {
+function showVideo(unitId) {
     const videoText_u509 = document.getElementById(`video-text_u${unitId}`);
     console.log(videoText_u509);
-    const unit_div=document.getElementById('unit-div');
-    const videoIframeContainer_u509 = document.getElementById(`video-iframe-container_u${unitId}`);
+    // todo  auto performypiutube search 
+    const linkContainer = document.querySelector('.link-container');
+    // console.log(linkContainer);
+    const topic = videoText_u509.textContent;
+    // console.log(topic);
+    linkContainer.outerHTML = 
+    `<div class="link-container-01">
+    <a href="https://www.youtube.com/results?search_query=${topic} Tamil"><i class="fa-brands fa-youtube fa-fade"
+            style="color: #e01b24;"></i>For Tamil</a>
+    <a href="https://www.youtube.com/results?search_query=${topic} English"><i class="fa-brands fa-youtube fa-fade"
+            style="color: #e01b24;"></i>For English</a>
+    </div>
+    <p style="color:black;">Scroll Last to Download PDF<a href="#pdf"><i class="fa-solid fa-file-pdf"
+            id="pdf"></i></a>
+    </p>`;
 
+    // todo end youtube search
+    const unit_div = document.getElementById('unit-div');
+    const videoIframeContainer_u509 = document.getElementById(`video-iframe-container_u${unitId}`);
     videoText_u509.addEventListener("click", function () {
         if (videoIframeContainer_u509.style.display === "none") {
             videoIframeContainer_u509.style.display = "block";
@@ -24,16 +40,18 @@
 
 // todo Function for loop the unit
 
-function loopUnit(unit_count,Topics_count){
-    console.log(typeof(unit_count),typeof(Topics_count));
-    for(let i =1;i <= Topics_count ; i++){
-        i <=9 ? showVideo(unit_count + '0' + i): showVideo(unit_count  + '' + i);
+function loopUnit(unit_count, Topics_count) {
+    // console.log(typeof(unit_count),typeof(Topics_count));
+    for (let i = 1; i <= Topics_count; i++) {
+        i <= 9 ? showVideo(unit_count + '0' + i) : showVideo(unit_count + '' + i);
     }
 };
 
 //   ?    Unit Loop Function Call 
- loopUnit(1,16);
- loopUnit(2,13);
- loopUnit(3,14);
- loopUnit(4,10);
- loopUnit(5,16);
+loopUnit(1, 16);
+loopUnit(2, 13);
+loopUnit(3, 14);
+loopUnit(4, 10);
+loopUnit(5, 16);
+
+
