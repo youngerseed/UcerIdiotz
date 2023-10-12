cb8d81b197407524266ded6379be0a463a0cb9ba
Devsk001 <srik3713@gmail.com>
2023-10-12 14:18:14 +0530

    ph-3254 updated to New Funcitonality Sem-02 Cse Finished

--- html/Department/CSE/Semester-02/js/PH-3254.js
+++ html/Department/CSE/Semester-02/js/PH-3254.js
@@ -1,10 +1,68 @@
+
+
+//  todo funciton for create and add the topics to the respective division
+
+function createDivision(unit_color,string,unitId,count){
+    const units_div = document.querySelector(`.${unit_color}`);
+   
+     units_div.innerHTML += `
+        <div class="video-container" id="video-container">
+                        <p id="video-text_u${unitId}0${count+1}" class="video-text">${string}</p>
+                        <!--          iframe tag -->
+                        <div id="video-iframe-container_u${unitId}0${count+1}"
+                            class="video-iframe-container col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12"
+                            style="display: none;">
+                            <div class="link-container">
+                                <a href="#"><i class="fa-brands fa-youtube fa-fade"
+                                        style="color: #e01b24;"></i>For Tamil</a>
+                                <a href="#"><i class="fa-brands fa-youtube fa-fade"
+                                        style="color: #e01b24;"></i>For English</a>
+                            </div>
+                            
+                        </div>
+        </div>
+        `;
+};
+  
+// todo  funciton for loop the topics and add to html
+
+ function loopTopics(split_text,divClolor,unit_count){
+    for(let count =0 ; count <split_text.length ; count++){
+        createDivision(divClolor ,split_text[count],unit_count,count);
+    }
+};
+// todo  function for seperate the text 
+
+ function Seperate_text(topics){
+  let split_text = [];
+  split_text = topics.split('-');
+  return split_text;
+};
+
+// todo   function for adding the papper code and title  
+
+ function Add_Papper_Code(Papper_code,semester_count,title){
+  const Papper_codes =document.querySelector('.header');
+  const semester_counts =document.querySelector('.unit');
+//   changingthe title of the page
+  document.title = title
+// changing papper code and papper name
+  Papper_codes.innerHTML = ` <p>${Papper_code}</p>`;
+  semester_counts.innerHTML = `  <i class="fa-solid fa-graduation-cap "></i>
+  <span>0${semester_count}</span>`; 
+};
+
 // todo ShowVideo Funciton 
 
-function showVideo(unitId) {
+ function showVideo(unitId) {
+
+    // todo unit topics automate js
     const videoText_u509 = document.getElementById(`video-text_u${unitId}`);
     console.log(videoText_u509);
+
     // todo  getting the papper code and name to perform search
     const papper_id = document.querySelector('.header').innerText;
+
     // todo  auto performypiutube search 
     const linkContainer = document.querySelector('.link-container');
     // console.log(linkContainer);
@@ -21,17 +79,22 @@ function showVideo(unitId) {
     </i>
     ImportantQuestions</a>
     </div>
-    <p style="color:black;">Scroll Last to Download PDF<a href="#pdf"><i class="fa-solid fa-file-pdf"
-            id="pdf"></i></a>
-    </p>`;
+   `;
+    // linkContainer.classList.add('link-container');
 
     // todo end youtube search
+
+    // todo addind the hover effect and onclick event
+
     const unit_div = document.getElementById('unit-div');
+    const video_container =document.getElementById('video-container');
+   
     const videoIframeContainer_u509 = document.getElementById(`video-iframe-container_u${unitId}`);
     videoText_u509.addEventListener("click", function () {
         if (videoIframeContainer_u509.style.display === "none") {
             videoIframeContainer_u509.style.display = "block";
             unit_div.classList.add("unit-div-2");
+            video_container.style.textAlign = 'center';
             videoIframeContainer_u509.classList.remove("hoverback");
         } else {
             unit_div.classList.remove("unit-div-2");
@@ -39,26 +102,87 @@ function showVideo(unitId) {
             setTimeout(function () {
                 videoIframeContainer_u509.style.display = "none";
             }, 1000)
-        }
+        };
     });
 };
 
 
 // todo Function for loop the unit
 
-function loopUnit(unit_count, Topics_count) {
+ function loopUnit(unit_count, Topics_count) {
     // console.log(typeof(unit_count),typeof(Topics_count));
     for (let i = 1; i <= Topics_count; i++) {
-        i <= 9 ? showVideo(unit_count + '0' + i) : showVideo(unit_count + '' + i);
+        // i <= 9 ? showVideo(unit_count + '0' + i) : showVideo(unit_count + "" + i);
+       showVideo(unit_count + '0' + i); 
     }
 };
 
-//   ?    Unit Loop Function Call 
-loopUnit(1, 16);
-loopUnit(2, 13);
-loopUnit(3, 14);
-loopUnit(4, 10);
-loopUnit(5, 16);
+ function Unit_loop_function(text , unit_color ,unit_count){
+
+    const Unit_topics =text;
+    loopTopics(Seperate_text(Unit_topics),unit_color,unit_count);
+    loopUnit(unit_count,Seperate_text(Unit_topics).length);
+
+};
+// **********************    End of the all functions ********************   
+
+// todo   Unit_color List Dont Change this 
+
+const unit_01_color = 'bd-blue';
+const unit_02_color = 'bd-yellow';
+const unit_03_color = 'bd-green';
+const unit_04_color = 'bd-pink' ;
+const unit_05_color = 'bd-orange';
+
+// todo Unit text  Change Input text
+
+
+let unit_text1 = ` Classical free electron theory - Expression for electrical conductivity - Thermal conductivity
+expression - Wiedemann Franz law - electrons in metals - Particle in a
+three dimensional box - degenerate states - Fermi Dirac statistics - Density of energy states -
+Electron in periodic potential - Energy bands in solids - tight binding approximation - Electron
+effective mass - concept of hole `;
+
+let unit_text2 = ` Intrinsic Semiconductors - Energy band diagram - direct and indirect band gap semiconductors -
+Carrier concentration in intrinsic semiconductors - extrinsic semiconductors - Carrier concentration
+in N type - Carrier concentration P type semiconductors - Variation of carrier concentration with temperature - variation
+of Fermi level with temperature and impurity concentration - 
+random motion,drift, mobility and diffusion - Hall effect and devices - Ohmic contacts - Schottky
+diode *`;
+
+let unit_text3 = ` Magnetic dipole moment - atomic magnetic moments- magnetic permeability and susceptibility - diamagnetism - paramagnetism - ferromagnetism -
+antiferromagnetism - ferrimagnetism - Ferromagnetism origin and exchange interaction- saturation
+magnetization and Curie temperature - Domain Theory- M versus H behaviour - Hard and soft
+magnetic materials - Magnetic principle in computer data storage - Magnetic
+hard disc (GMR sensor).`;
+
+let unit_text4 = ` Classification of optical materials - carrier generation and recombination processes - Absorption
+emission and scattering of light in metals -  insulators and semiconductors  - photo
+current in a P N diode - solar cell - LED - Organic LED - Laser diodes - Optical data storage
+techniques `;
+
+let unit_text5 = ` quantum confinement -  quantum wells ,wires , dots - band
+gap of nanomaterials - Tunneling - Single electron phenomena: Coulomb blockade - resonant Tunneling diode - single electron transistor - quantum cellular automata - Quantum system for
+information processing - quantum states - classical bits - quantum bits or qubits - CNOT gate -
+multiple qubits - Bloch sphere - quantum gates  `;
+
+
+
+
+// todo Cont Change the Fucniton Call 
+Unit_loop_function(unit_text1 , unit_01_color , 1);
+Unit_loop_function(unit_text2 , unit_02_color , 2);
+Unit_loop_function(unit_text3 , unit_03_color , 3);
+Unit_loop_function(unit_text4 , unit_04_color , 4);
+Unit_loop_function(unit_text5 , unit_05_color , 5);
+
+
+// ****************************  End Of the Funciton call *****************
+
+ 
+  
+
+
 
 
 
